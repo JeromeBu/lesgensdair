d06041876111940d5c9879d465abcbc40b5181b5
Jerome Burkard <jerome.burkard@gadz.org>
2017-04-17 19:50:48 +0200

    change inge asset compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,8 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "http://TODO_PUT_YOUR_DOMAIN_HERE" }
+
   config.serve_static_assets = true
-  config.assets.compile = true
+
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -26,7 +27,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
