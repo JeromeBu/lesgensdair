17458ec0560c893845647f8ecaeb9218411a6548
Jerome Burkard <jerome.burkard@gadz.org>
2017-04-18 19:19:41 +0200

    regexp pour email

--- app/models/order.rb
+++ app/models/order.rb
@@ -3,6 +3,7 @@ class Order < ApplicationRecord
 
   validates :first_name, presence: { message: "Veuillez mettre le prénom de la personne qui volera" }
   validates :last_name, presence: { message: "Veuillez mettre le nom de la personne qui volera" }
-  validates :email, presence: { message: "Veuillez mettre un email valide" }
+  validates :email, presence: { message: "Veuillez mettre un email" }
+  validates :email, format: {:with => /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, :message => "Veuillez renseigner un email valide" }
 
 end
